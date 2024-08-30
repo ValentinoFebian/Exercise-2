faddf14b357c5d3fa9f7425b58e349faa29260bc
ValentinoFebian <valentinocfebian@gmail.com>
2024-08-30 14:34:51 +0700

    feat: exercise2 correction (prime problem)

--- free-exercise2.js
+++ free-exercise2.js
@@ -16,6 +16,30 @@ console.log(`${number} is odd number!`);
 // Example: 7 → 7 is a prime number
 // Example: 6 → 6 is not a prime number
 
+let randomNum = 15;
+let primeChecker;
+
+if (randomNum <1) {
+    console.log("This is not prime number");
+} else if (randomNum === 1 || randomNum === 2 ) {
+    console.log("This is prime number")
+} else {
+    for (let i = 2; i < randomNum/ 2; i++) {
+        if (randomNum % i === 0) {
+            primeChecker = false
+            break
+        }
+    }
+    if (primeChecker === true) {
+        console.log("This is prime number");
+    } else {
+        console.log("This is not prime number");
+    }
+}
+    
+
+/*
+// Old Version
 let randomNum = 15
 
 if (randomNum < 1) {
@@ -28,14 +52,16 @@ if (randomNum < 1) {
     for (let i = 2; i < randomNum; i++) {
         if (randomNum % i === 0) {
             console.log("This is not prime number");
-            break
+            
         } else {
             console.log("This is prime number")
-            break
+            
         }
     }
     
 } 
+*/
+
 
 // Write a code to find the sum of the numbers 1 to N
 // Example: 5 → 1 + 2 + 3 + 4 + 5 = 15
@@ -80,3 +106,4 @@ for (i = 3; i <= numFib; i++ ) {
 }
 
 console.log(c)
+jkasxccas
